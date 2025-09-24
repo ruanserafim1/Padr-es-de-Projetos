1f362c50ae2526bd3fe66a2aced3d546b7d4f619
ruanserafim1 <141503121+ruanserafim1@users.noreply.github.com>
2025-09-24 07:16:09 -0300

    Delete PADROES directory

--- PADROES/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "HidrometroApp.h"
-#include <QApplication>
-
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
-    HidrometroApp window;
-    window.show();
-    return app.exec();
-}
